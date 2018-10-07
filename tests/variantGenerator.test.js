f13e9afbdc9f2734ca1bbbe809faace86a16c139
Ron Melkhior <ron3353@gmail.com>
2018-10-08 00:05:36 +0300

    Added tests

---
+++ tests/variantGenerator.test.js
@@ -0,0 +1,45 @@
+const postcss = require('postcss');
+const tailwindcss = require('tailwindcss');
+const config = require('./config');
+
+test('it generates direction variants', () => {
+	const input = `
+		@variants direction {
+			.banana { color: yellow; }
+			.tomato { color: red; }
+		}
+	`;
+
+	const output = `
+		.banana {
+			color: yellow;
+		}
+
+		.tomato {
+			color: red;
+		}
+
+		[dir='ltr'] .ltr\\:banana {
+			color: yellow;
+		}
+
+		[dir='ltr'] .ltr\\:tomato {
+			color: red;
+		}
+
+		[dir='rtl'] .rtl\\:banana {
+			color: yellow;
+		}
+
+		[dir='rtl'] .rtl\\:tomato {
+			color: red;
+		}
+	`;
+
+	return postcss([tailwindcss(config)])
+		.process(input, { from: undefined })
+		.then(result => {
+			expect(result.css).toMatchCss(output);
+			expect(result.warnings().length).toBe(0);
+		});
+});
