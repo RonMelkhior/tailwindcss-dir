c98ab024f60def8845a7b1d4e2205f0fd25369ca
Kevin Batdorf <kbat82@gmail.com>
2019-12-12 23:41:57 +0700

    add same level option

--- tests/variantGenerator.test.js
+++ tests/variantGenerator.test.js
@@ -1,6 +1,7 @@
 const postcss = require('postcss');
 const tailwindcss = require('tailwindcss');
-const config = require('./config');
+const defaultConfig = require('tailwindcss/defaultConfig');
+const plugin = require('../src/index');
 
 test('it generates direction variants', () => {
 	const input = `
@@ -40,8 +41,56 @@ test('it generates direction variants', () => {
 		}
 	`;
 
-	return postcss([tailwindcss(config)])
+	return postcss([tailwindcss({ ...defaultConfig, plugins: [plugin()] })])
 		.process(input, { from: undefined })
+		.then(result => {
+			expect(result.css).toMatchCss(output);
+			expect(result.warnings().length).toBe(0);
+		});
+});
+
+test('it generates direction variants without same level', () => {
+	const input = `
+		@variants direction {
+			.banana { color: yellow; }
+			.tomato { color: red; }
+		}
+	`;
+
+	const output = `
+		.banana {
+			color: yellow;
+		}
+
+		.tomato {
+			color: red;
+		}
+
+		[dir='ltr'] .ltr\\:banana {
+			color: yellow;
+		}
+
+		[dir='ltr'] .ltr\\:tomato {
+			color: red;
+		}
+
+		[dir='rtl'] .rtl\\:banana {
+			color: yellow;
+		}
+
+		[dir='rtl'] .rtl\\:tomato {
+			color: red;
+		}
+	`;
+
+	return postcss([tailwindcss({
+		...defaultConfig,
+		plugins: [
+			plugin({
+				sameLevel: false
+			})
+		]
+	})]).process(input, { from: undefined })
 		.then(result => {
 			expect(result.css).toMatchCss(output);
 			expect(result.warnings().length).toBe(0);
